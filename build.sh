884697203c24e91a9cf889827a8ecbb7a83d46b9
yangmichaely <yangmichaely@gmail.com>
2024-04-01 22:03:13 -0500

    build.sh experimetnation

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
-rm -rf ./libtdmm/libtdmm.a
-rm -rf build
-cmake -S . -B build
-make -C build
-mv ./build/libtdmm/libtdmm.a ./libtdmm
+rm -rf ./build
+cmake -S . -B ./build
+make -C ./build
